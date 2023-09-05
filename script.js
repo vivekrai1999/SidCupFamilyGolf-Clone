d2d911693188256b00195b14e1de8e80a9c0cdc6
vivekrai1999 <vivek.rai.2807@gmail.com>
2023-09-05 14:04:14 +0530

    Removed Markers

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ gsap.to("#nav",{
     scrollTrigger:{
         trigger: "#nav",
         scroller: "body",
-        markers: true,
         start: "top -10%",
         end: "top -11%",
         scrub: 1
